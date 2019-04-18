cf144aac9aebcb2cd28a4d3393666b19893ba55a
Maksym Stepanenko <maksym.stepanenko@opower.com>
2019-04-18 12:55:15 +0300

    Reading react component state.

--- src/components/App.js
+++ src/components/App.js
@@ -2,13 +2,57 @@ import React from 'react';
 import Header from './Header';
 import ContestPreview from './ContestPreview';
 
+const data =
+    {
+        "contests": [
+            {
+                "id": 1,
+                "categoryName": "Business/Company",
+                "contestName": "Cognitive Building Bricks"
+            },
+            {
+                "id": 2,
+                "categoryName": "Magazine/Newsletter",
+                "contestName": "Educating people about sustainable food production"
+            },
+            {
+                "id": 3,
+                "categoryName": "Software Component",
+                "contestName": "Big Data Analytics for Cash Circulation"
+            },
+            {
+                "id": 4,
+                "categoryName": "Website",
+                "contestName": "Free programming books"
+            }
+        ]
+    };
+
 class App extends React.Component {
+
+    state = {
+        contests: []
+    };
+
+    componentDidMount() {
+
+        console.log('Sending AJAX call');
+
+        setTimeout(() => {
+            this.setState({contests: data.contests});
+            console.log('AJAX completed');
+        }, 3000);
+    }
+
+    componentWillUnmount() {
+        //todo:
+    }
 
     render() {
         return (
             <div className='App'>
                 <Header message='Naming Contests'/>
-                {this.props.contests.map(singleContest =>
+                {this.state.contests.map(singleContest =>
                     <ContestPreview key={singleContest.id} {...singleContest}/>
                 )}
             </div>
